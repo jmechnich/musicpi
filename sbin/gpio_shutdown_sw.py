5a1cda2ef2da4d25c6e50ab6fd63edc9ed441d66
jmechnich <joerg.mechnich@gmail.com>
2014-11-02 11:02:29 +0100

    leave LED on when terminated/killed

--- sbin/gpio_shutdown_sw.py
+++ sbin/gpio_shutdown_sw.py
@@ -12,8 +12,8 @@ def blink():
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-atexit.register(GPIO.cleanup)
-signal.signal(signal.SIGINT,lambda x, y: sys.exit(0))
+#atexit.register(GPIO.cleanup)
+signal.signal(signal.SIGINT,lambda x, y: [GPIO.cleanup(), sys.exit(0)])
 
 while True:
         if GPIO.input(17) == 0:
