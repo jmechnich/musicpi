9d89ba0f3dc57a62af22afae4abf6b04ac8b77f6
jmechnich <joerg.mechnich@gmail.com>
2015-05-31 20:22:36 +0200

    merged carpi changes, use hostname wherever possible

--- install.sh
+++ install.sh
@@ -32,8 +32,6 @@ for f in bin/* sbin/*;  do do_symlink "$PWD/$f" /usr/local/$f;  done
 
 do_symlink "$PWD/etc/interfaces"      /etc/network/interfaces
 do_symlink "$PWD/etc/dnsmasq.conf"    /etc/dnsmasq.conf.musicpi
-do_symlink "$PWD/etc/dnsmasq.hosts"   /etc/dnsmasq.hosts
-do_symlink "$PWD/etc/hostapd.conf"    /etc/hostapd.conf.musicpi
 do_copy    "$PWD/etc/smbd.service"    /etc/avahi/services/smbd.service
 do_copy    "$PWD/etc/start_gpxlogger" /etc/cron.d/start_gpxlogger
 do_copy    "$PWD/etc/start_gpspipe"   /etc/cron.d/start_gpspipe
@@ -48,3 +46,13 @@ else
     echo 'exit 0' | sudo tee -a /etc/rc.local
 fi
 
+sudo dd of=/etc/dnsmasq.hosts status=none <<EOF
+192.168.178.1 `hostname`.localnet `hostname`
+EOF
+
+sudo dd of=/etc/hostapd.conf.musicpi status=none <<EOF
+interface=wlan0
+driver=rtl871xdrv
+ssid=`hostname`
+channel=6
+EOF
